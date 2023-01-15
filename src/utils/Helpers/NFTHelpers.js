f4d73706b796e797edab42f35e6f433331370fe2
Ghonghito <73051326+Ghonghito@users.noreply.github.com>
2023-01-15 13:33:33 +0400

    Update NFTHelpers.js

--- src/utils/Helpers/NFTHelpers.js
+++ src/utils/Helpers/NFTHelpers.js
@@ -9,93 +9,56 @@ export const fetchNFTImage = async (url) => {
     .then((response) => response)
     .catch((error) => error.response)
 
-  if (json.headers['content-type'].includes('image')) {
-    return url
+  if (json.status === 200) {
+    if (json.headers['content-type'].includes('image')) return url
+    if (json.hasOwnProperty('data')) {
+      if (String(json.data.image).startsWith('ipfs://')) {
+        return String(json.data.image).replace('ipfs://', 'https://ipfs.io/ipfs/')
+      } else {
+        return json.data.image
+      }
+    } else {
+      return QuestionMark
+    }
   } else {
-    return json.hasOwnProperty('data') ? json.data.image : QuestionMark
+    return QuestionMark
   }
 }
 
+export const convertToIpfsUrl = (url) => {
+  return String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
+}
+
+const imageExtensions = ['.jpeg', '.png', '.gif', '.jpg', '.webp']
+
 export const getNFTImage = async (url) => {
-  if (String(url).startsWith('ipfs://')) {
-    if (String(url).endsWith('.jpeg')) {
-      return String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
-    } else if (String(url).endsWith('.png')) {
-      return String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
-    } else if (String(url).endsWith('.gif')) {
-      return String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
-    } else if (String(url).endsWith('.jpg')) {
-      return String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
-    } else if (String(url).endsWith('.json')) {
-      const formatedUrl = String(url).replace('ipfs://', 'https://ipfs.io/ipfs/')
-      const test = await fetchNFTImage(formatedUrl)
-      return String(test).replace('ipfs://', 'https://ipfs.io/ipfs/')
+  if (url.startsWith('ipfs://')) {
+    let formattedUrl = url.replace('ipfs://', 'https://ipfs.io/ipfs/')
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return formattedUrl
     } else {
-      return await fetchNFTImage(url)
+      return await fetchNFTImage(convertToIpfsUrl(url))
     }
   }
 
   if (String(url).startsWith('https://ipfs.io/ipfs/')) {
-    if (String(url).endsWith('.jpeg')) {
-      return url
-    } else if (String(url).endsWith('.png')) {
-      return url
-    } else if (String(url).endsWith('.gif')) {
-      return url
-    } else if (String(url).endsWith('.jpg')) {
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
       return url
     } else {
       return await fetchNFTImage(url)
     }
-  }
-
-  if (String(url).startsWith('https://metadata.wav3.net/token/')) {
-    return await fetchNFTImage(url)
-  }
-
-  if (String(url).includes('https://www.aptosnames.com/')) {
-    const result = String(url).replace('/mainnet', '').replace('metadata', 'image')
-    return result
   }
 
   if (String(url).startsWith('https://nftstorage.link/ipfs/')) {
-    if (String(url).endsWith('.jpeg')) {
-      return url
-    } else if (String(url).endsWith('.png')) {
-      return url
-    } else if (String(url).endsWith('.gif')) {
-      return url
-    } else if (String(url).endsWith('.jpg')) {
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
       return url
     } else {
       return await fetchNFTImage(url)
     }
   }
 
-  if (String(url).startsWith('https://cloudflare-ipfs.com/ipfs/')) {
-    return await fetchNFTImage(url)
-  }
-
-  if (String(url).includes('.arweave.net')) {
-    return url
-  }
-
-  if (String(url).startsWith('https://arweave.net/')) {
-    return url
-  }
-
-  if (String(url).includes('https://lime-worried-crawdad-244.mypinata.cloud/ipfs/')) {
-    return String(url).replace('https://lime-worried-crawdad-244.mypinata.cloud/ipfs/', 'https://ipfs.io/ipfs/')
-  }
-
   if (String(url).includes('.ipfs.dweb.link')) {
-    if (String(url).endsWith('.jpeg')) {
-      return url
-    } else if (String(url).endsWith('.png')) {
-      return url
-    } else if (String(url).endsWith('.gif')) {
-      return url
-    } else if (String(url).endsWith('.jpg')) {
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
       return url
     } else {
       return await fetchNFTImage(url)
@@ -103,52 +66,56 @@ export const getNFTImage = async (url) => {
   }
 
   if (String(url).startsWith('https://static.bluemove.net/')) {
-    if (String(url).endsWith('.webp')) return url
-    if (String(url).endsWith('.png')) return url
-    if (String(url).endsWith('.jpg')) return url
-    if (String(url).endsWith('.gif')) return url
-    if (String(url).endsWith('.json')) return await fetchNFTImage(url)
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return url
+    } else {
+      return await fetchNFTImage(url)
+    }
   }
 
   if (String(url).startsWith('https://bluemovecdn')) {
-    if (String(url).endsWith('.webp')) return url
-    if (String(url).endsWith('.png')) return url
-    if (String(url).endsWith('.jpg')) return url
-    if (String(url).endsWith('.gif')) return url
-    if (String(url).endsWith('.json')) return await fetchNFTImage(url)
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return url
+    } else {
+      return await fetchNFTImage(url)
+    }
   }
 
   if (String(url).startsWith('https://cdn.bluemove')) {
-    if (String(url).endsWith('.webp')) return url
-    if (String(url).endsWith('.png')) return url
-    if (String(url).endsWith('.jpg')) return url
-    if (String(url).endsWith('.gif')) return url
-    if (String(url).endsWith('.json')) return await fetchNFTImage(url)
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return url
+    } else {
+      return await fetchNFTImage(url)
+    }
   }
 
   if (String(url).startsWith('https://ipfs.bluemove')) {
-    if (String(url).endsWith('.webp')) return url
-    if (String(url).endsWith('.png')) return url
-    if (String(url).endsWith('.jpg')) return url
-    if (String(url).endsWith('.gif')) return url
-    if (String(url).endsWith('.json')) return await fetchNFTImage(url)
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return url
+    } else {
+      return await fetchNFTImage(url)
+    }
   }
 
-  if (String(url).endsWith('.png')) {
-    return url
+  if (String(url).startsWith('https://cloudflare-ipfs.com/ipfs/')) {
+    if (imageExtensions.some((ext) => url.endsWith(ext))) {
+      return url
+    } else {
+      return await fetchNFTImage(url)
+    }
   }
 
-  if (String(url).endsWith('.gif')) {
-    return url
-  }
+  if (String(url).startsWith('https://metadata.wav3.net/token/')) return await fetchNFTImage(url)
 
-  if (String(url).endsWith('.jpg')) {
-    return url
-  }
+  if (String(url).includes('https://www.aptosnames.com/')) return String(url).replace('/mainnet', '').replace('metadata', 'image')
 
-  if (String(url).endsWith('.jpeg')) {
-    return url
-  }
+  if (String(url).includes('.arweave.net')) return url
+
+  if (String(url).startsWith('https://arweave.net/')) return url
+
+  if (String(url).includes('https://lime-worried-crawdad-244.mypinata.cloud/ipfs/')) return String(url).replace('https://lime-worried-crawdad-244.mypinata.cloud/ipfs/', 'https://ipfs.io/ipfs/')
+
+  if (imageExtensions.some((ext) => url.endsWith(ext))) return url
 
   return QuestionMark
 }
